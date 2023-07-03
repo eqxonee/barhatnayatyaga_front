b64efc4359ba957f56b05670fb36926ca715795c
Sergey Vandushev <s.vandushev@of.ru>
2023-07-03 15:14:33 +0300

    add modal button

--- src/components/modal/ModalButton.jsx
+++ src/components/modal/ModalButton.jsx
@@ -1,27 +1,31 @@
-import {Button, Image, Modal} from 'antd';
+import {Button, Form, Image, Input, InputNumber, Modal} from 'antd';
 import {useState} from 'react';
 import React from 'react';
 
 const ModalButton = () => {
     const [modalIsOpen, setModalOpen] = useState(false);
+    let [name, setName] = useState("");
     return (
         <>
             <Button type="primary" onClick={() => setModalOpen(true)}>
-                модал очка
+                Забронировать стол
             </Button>
             <Modal
-                title="Окно с очень важной информацией"
+                title="Бронирование столика"
                 centered
                 open={modalIsOpen}
                 onOk={() => setModalOpen(false)}
                 onCancel={() => setModalOpen(false)}
                 okText="Переспать с ним"
                 cancelText="Остаться никем">
-                <p>Вы совершили ахуенно много действий</p>
-                <Image
-                    width={'100%'}
-                    src="https://foni.club/uploads/posts/2023-03/1677740962_foni-club-p-piksel-art-billi-kherrington-3.jpg"
-                />
+                <Form.Item> <Input style={{ width: 480 }} placeholder="Ввести че нибудь" /> </Form.Item>
+                <Form.Item> <Input style={{ width: 480 }} placeholder="Ввести че нибудь" /> </Form.Item>
+                <Form.Item> <Input style={{ width: 480 }} placeholder="Ввести че нибудь" /> </Form.Item>
+                <Form.Item> <Input style={{ width: 480 }} placeholder="Ввести че нибудь" /> </Form.Item>
+                <Form.Item> <InputNumber style={{ width: 480 }} placeholder="Ввести число" /> </Form.Item>
+
+
+
             </Modal>
         </>
     );
